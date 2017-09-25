3a316e60f24f0489d3bdaf145c78f4432bab62b8
jsongo <jsongo@qq.com>
2017-09-25 18:44:27 +0800

    update range to detect changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import {
+    Component, ViewContainerRef,
+    OnInit, Injector,
+    ComponentFactoryResolver
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    min = 1;
     max = 10;
     constructor() {
         setTimeout(() => {
-            this.max = 20;
-        }, 1000);
+            this.min = 3;
+            this.max = 16;
+        }, 2000);
+    }
+
+    ngOnInit() {
     }
 }
